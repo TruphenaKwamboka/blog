c4df5a31d9e38ad147354f777b1151334668c927
Tru <trusoro32@gmail.com>
2023-04-21 06:22:43 +0300

    header

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -3,7 +3,14 @@ import "./header.css"
 export default function Header() {
   return (
     <div className="header">
-        header
+    <div className="headerTitles">
+      <span className="headerTitlesSm">React & Node</span>
+      <span className="headerTitleLg">Blog</span>
+    </div>
+    <img 
+    className="headerImg" 
+    src="imageh.jpg" 
+    alt="" />
     </div>
   )
 }
